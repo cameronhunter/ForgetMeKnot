d3d55f57524e447b4aead0a33212f8285facfa78
Cameron Hunter <hello@cameronhunter.co.uk>
2012-09-17 23:47:11 +0100

    Using a dialog view to minimise the screen real estate required.

--- src/uk/co/cameronhunter/forgetmeknot/MainActivity.java
+++ src/uk/co/cameronhunter/forgetmeknot/MainActivity.java
@@ -1,14 +1,16 @@
 package uk.co.cameronhunter.forgetmeknot;
 
 import static android.content.Intent.ACTION_INSERT;
+
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity {
 
@@ -18,27 +20,37 @@ public class MainActivity extends Activity {
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 
+        final Random random = new Random();
+        final String[] reminders = getResources().getStringArray( R.array.precanned_reminders );
+
         setContentView( R.layout.main );
 
-        EditText input = (EditText) findViewById( R.id.input );
-        input.setOnEditorActionListener( new OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction( TextView v, int actionId, KeyEvent event ) {
-                if ( EditorInfo.IME_ACTION_DONE != actionId ) return false;
+        final EditText input = (EditText) findViewById( R.id.input );
+        input.setHint( reminders[random.nextInt( reminders.length )] );
 
-                String reminderText = v.getText().toString();
-                if ( reminderText != EMPTY_STRING ) {
+        Button save = (Button) findViewById( R.id.save );
+        Button cancel = (Button) findViewById( R.id.cancel );
+
+        save.setOnClickListener( new OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                String reminderText = input.getText().toString();
+                if ( reminderText.length() > 0 ) {
                     Intent createReminder = new Intent( ACTION_INSERT );
                     createReminder.putExtra( getString( R.string.reminder_text ), reminderText );
-                    
-                    v.setText( EMPTY_STRING );
-                    
-                    sendBroadcast( createReminder );
-                    
-                    return true;
-                }
 
-                return false;
+                    input.setHint( reminders[random.nextInt( reminders.length )] );
+                    input.setText( EMPTY_STRING );
+
+                    sendBroadcast( createReminder );
+                }
+            }
+        } );
+
+        cancel.setOnClickListener( new OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                finish();
             }
         } );
     }
