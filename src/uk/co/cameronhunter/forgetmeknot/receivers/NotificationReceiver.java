7bc9aee124c062d18557afd029f77f6b453248e8
cameron <cameron@EDI-6900034820.ant.amazon.com>
2012-09-18 23:11:51 +0100

    Using BigStyleNotification to allow for longer reminder messages. Allowing for line breaks in the reminder text.

--- src/uk/co/cameronhunter/forgetmeknot/receivers/NotificationReceiver.java
+++ src/uk/co/cameronhunter/forgetmeknot/receivers/NotificationReceiver.java
@@ -9,7 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.BigTextStyle;
 import android.support.v4.app.NotificationCompat.Builder;
 
 public class NotificationReceiver extends BroadcastReceiver {
@@ -17,25 +17,25 @@ public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive( Context context, Intent intent ) {
         if ( intent == null || intent.getExtras() == null ) return;
-        
+
         Resources resources = context.getResources();
         NotificationManager notificationManager = (NotificationManager) context.getSystemService( NOTIFICATION_SERVICE );
 
         long reminderId = intent.getLongExtra( context.getString( R.string.reminder_id ), -1 );
         String reminderText = intent.getStringExtra( context.getString( R.string.reminder_text ) );
-        
+
         Reminder reminder = new Reminder( reminderId, reminderText );
-        
-        Builder builder = new NotificationCompat.Builder( context );
-        
+
         Intent removeIntent = new Intent( Intent.ACTION_DELETE );
         removeIntent.putExtra( context.getString( R.string.reminder_id ), reminder.id );
 
-        builder.setSmallIcon( R.drawable.logo2 ) //
-                .setContentText( reminder.text ) //
-                .setContentTitle( resources.getText( R.string.reminder_title ) ) //
-                .setTicker( resources.getText( R.string.reminder_ticker ) ) //
-                .setDeleteIntent( PendingIntent.getBroadcast( context, reminder.id.intValue(), removeIntent, 0 ) );
+        BigTextStyle builder = new BigTextStyle( //
+                new Builder( context ).setSmallIcon( R.drawable.logo2 ) //
+                        .setContentText( reminder.text ) //
+                        .setContentTitle( resources.getText( R.string.reminder_title ) ) //
+                        .setTicker( resources.getText( R.string.reminder_ticker ) ) //
+                        .setDeleteIntent( PendingIntent.getBroadcast( context, reminder.id.intValue(), removeIntent, 0 ) ) //
+        ).bigText( reminder.text );
 
         notificationManager.notify( reminder.id.intValue(), builder.build() );
     }
