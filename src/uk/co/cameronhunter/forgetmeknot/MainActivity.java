7fd0e56e02380b6265febcceaac980045c46933c
Cameron Hunter <hello@cameronhunter.co.uk>
2012-09-19 21:40:18 +0100

    Small refactor of the MainActivity

--- src/uk/co/cameronhunter/forgetmeknot/MainActivity.java
+++ src/uk/co/cameronhunter/forgetmeknot/MainActivity.java
@@ -18,14 +18,12 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    private static final String EMPTY_STRING = "";
-
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 
         setTitle( getString( R.string.reminder_hint ) );
-        
+
         final Random random = new Random();
         final String[] placeholders = getResources().getStringArray( R.array.precanned_reminders );
 
@@ -37,18 +35,15 @@ public class MainActivity extends Activity {
         Intent intent = getIntent();
         final AtomicLong reminderId = new AtomicLong( intent.getLongExtra( getString( R.string.reminder_id ), -1 ) );
 
-         if ( ACTION_EDIT.equals( intent.getAction() ) ) {
+        if ( ACTION_EDIT.equals( intent.getAction() ) ) {
             Reminders data = new Reminders( this );
             Reminder reminder = data.find( reminderId.get() );
-            
-            if ( reminder == null ) {
-                reminderId.set( -1 );
-            } else {
+            if ( reminder != null ) {
                 input.setText( reminder.text );
                 input.setSelectAllOnFocus( true );
             }
         }
-        
+
         Button save = (Button) findViewById( R.id.save );
         Button cancel = (Button) findViewById( R.id.cancel );
 
@@ -58,18 +53,12 @@ public class MainActivity extends Activity {
                 String reminderText = input.getText().toString();
                 if ( reminderText.length() > 0 ) {
                     Intent createOrUpdateReminder = new Intent( ACTION_INSERT_OR_EDIT );
-                    
-                      createOrUpdateReminder.putExtra( getString( R.string.reminder_text ), reminderText );
-                    
-                    if ( reminderId.get() > -1 ) {
-                        createOrUpdateReminder.putExtra( getString( R.string.reminder_id ), reminderId.get() ); 
-                    }
 
-                    input.setHint( placeholders[random.nextInt( placeholders.length )] );
-                    input.setText( EMPTY_STRING );
+                    createOrUpdateReminder.putExtra( getString( R.string.reminder_id ), reminderId.get() );
+                    createOrUpdateReminder.putExtra( getString( R.string.reminder_text ), reminderText );
 
                     sendBroadcast( createOrUpdateReminder );
-                    
+
                     finish();
                 }
             }
